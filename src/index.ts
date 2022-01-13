75ebedaa47bc9be7700785c2d1fb9f821b31803d
zhushijie.jayson <zhushijie.jayson@bytedance.com>
2022-01-13 20:46:37 +0800

    feat: add github api

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+async function run() {
+  try {
+    const message = core.getInput('message');
+    const github_token = core.getInput('GITHUB_TOKEN');
+
+    console.log("core.getInput('message')......", '\n', message);
+    console.log("core.getInput('GITHUB_TOKEN')......", '\n', message);
+
+    const context = github.context;
+    if (context.payload.pull_request == null) {
+      core.setFailed('No pull request found.');
+      return;
+    }
+    console.log("context->", "\n", context);
+    const pull_request_number = context.payload.pull_request.number;
+    console.log("pull_request_number", "\n", context.payload.pull_request.number);
+
+    const octokit = github.getOctokit(github_token);
+    octokit.rest.pulls.createReviewComment({
+      ...context.repo,
+      issue_number: pull_request_number,
+      body: message
+    } as any)
+
+  } catch (error: any) {
+    core.setFailed(error.message);
+  }
+}
+
+// run();
+
+console.log(github)
+console.log(core)
